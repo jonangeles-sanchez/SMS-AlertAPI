c636ef63e46ea95ade1507fef7373b98af2b30ab
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 13:00:53 -0400

    Create ShoeRequest Model

---
+++ SMS-AlertAPI/Models/ShoeRequest.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS_AlertAPI.Models
+{
+    public class ShoeRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        private int PhoneNumber { get; set; }
+
+        public List<Shoe> requestedShoes { get; set; }
+
+    }
+}
