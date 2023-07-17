23fd1b60e9ed3bcf55ccfb17c35ec44fa01d0e43
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 14:22:58 -0400

    Create interface w/createRequest

---
+++ SMS-AlertAPI/Service/IShoeRequestService.cs
@@ -0,0 +1,10 @@
+﻿using SMS_AlertAPI.DTOs;
+using SMS_AlertAPI.Models;
+
+namespace SMS_AlertAPI.Service
+{
+    public interface IShoeRequestService
+    {
+        Task<List<ShoeRequest>> CreateRequest(ShoeRequest SRequest);
+    }
+}
