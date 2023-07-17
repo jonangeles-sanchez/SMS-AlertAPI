ace6a82b847db1bd2a82a5a14c0195e457d8ccea
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 13:00:44 -0400

    Create shoe model

---
+++ SMS-AlertAPI/Models/Shoe.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace SMS_AlertAPI.Models
+{
+    public class Shoe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public double Size { get; set; }
+
+        [JsonIgnore]
+        public ShoeRequest Request { get; set; }
+    }
+}
