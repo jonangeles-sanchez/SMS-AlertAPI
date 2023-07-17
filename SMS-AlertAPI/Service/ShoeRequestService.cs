5e024699ad08d6ab468a2cfa318c4713f41911a2
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 14:23:27 -0400

    Implement createRequest w/DynamoDb

---
+++ SMS-AlertAPI/Service/ShoeRequestService.cs
@@ -0,0 +1,21 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using SMS_AlertAPI.Models;
+
+namespace SMS_AlertAPI.Service
+{
+    public class ShoeRequestService : IShoeRequestService
+    {
+        IDynamoDBContext _dynamoDb;
+        public ShoeRequestService(IDynamoDBContext dynamoDBContext) 
+        { 
+            _dynamoDb = dynamoDBContext;
+        }
+
+        public async Task<List<ShoeRequest>> CreateRequest(ShoeRequest SRequest)
+        {
+            await _dynamoDb.SaveAsync(SRequest);
+            return await _dynamoDb.ScanAsync<ShoeRequest>(new List<ScanCondition>()).GetRemainingAsync();
+        }
+    }
+
+}
