733ea543cff19cd9f4bc733f40e7ff38778624ca
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 14:22:34 -0400

    Add 'Required' annotations

--- SMS-AlertAPI/DTOs/ShoeDTO.cs
+++ SMS-AlertAPI/DTOs/ShoeDTO.cs
@@ -1,11 +1,17 @@
-﻿namespace SMS_AlertAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS_AlertAPI.DTOs
 {
     public class ShoeDTO
     {
         public int Id { get; set; }
+        [Required]
         public int ShoeRequestId { get; set; }
+        [Required]
         public string Manufacturer { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public double Size { get; set; }
 
     }
--- SMS-AlertAPI/DTOs/ShoeRequestDto.cs
+++ SMS-AlertAPI/DTOs/ShoeRequestDto.cs
@@ -1,8 +1,11 @@
-﻿namespace SMS_AlertAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SMS_AlertAPI.DTOs
 {
     public class ShoeRequestDto
     {
         public int Id { get; set; }
+        [Required]
         public int PhoneNumber { get; set; }
         public List<ShoeDTO> requestedShoes { get; set; }
     }
