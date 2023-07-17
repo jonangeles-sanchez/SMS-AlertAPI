9abc14ecc5ed61884e82d8423cbbfd6aeed30cbb
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-17 13:30:47 -0400

    Create ShoeDTO

---
+++ SMS-AlertAPI/DTOs/ShoeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SMS_AlertAPI.DTOs
+{
+    public class ShoeDTO
+    {
+        public int ShoeRequestId { get; set; }
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Size { get; set; }
+
+    }
+}
