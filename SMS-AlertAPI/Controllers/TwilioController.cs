4470a3b59243679a371e76eb9cc5dabfba03cd99
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-18 11:54:17 -0400

    Create Twilio controller

---
+++ SMS-AlertAPI/Controllers/TwilioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Twilio.Clients;
+using SMS_AlertAPI.DTOs;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace SMS_AlertAPI.Controllers
+{
+    [Route("api/v1")]
+    [ApiController]
+    public class TwilioController : ControllerBase
+    {
+        private readonly ITwilioRestClient _client;
+        public TwilioController(ITwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        [HttpPost("PhoneNumber")]
+        public IActionResult SendSms(SmsMessageDTO req)
+        {
+            var message = MessageResource.Create(
+                body: req.Body,
+                from: new Twilio.Types.PhoneNumber(req.From),
+                to: new Twilio.Types.PhoneNumber(req.To),
+                client: _client);
+            return Ok("The following message was sent succesfully: \n" + message.Body);
+        }
+    }
+}
