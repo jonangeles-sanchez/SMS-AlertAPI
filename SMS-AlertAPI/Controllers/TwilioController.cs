eda2a37f7dd5830bbc4bb1a86aafe693945155c4
jonangeles-sanchez <70487639+jonangeles-sanchez@users.noreply.github.com>
2023-07-18 12:46:09 -0400

    Use appsettings phone number

--- SMS-AlertAPI/Controllers/TwilioController.cs
+++ SMS-AlertAPI/Controllers/TwilioController.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using SMS_AlertAPI.Util;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Twilio.Clients;
 using SMS_AlertAPI.DTOs;
 using Twilio.Rest.Api.V2010.Account;
+using Twilio.TwiML.Voice;
+using Twilio.Types;
 
 namespace SMS_AlertAPI.Controllers
 {
@@ -11,20 +14,28 @@ namespace SMS_AlertAPI.Controllers
     public class TwilioController : ControllerBase
     {
         private readonly ITwilioRestClient _client;
-        public TwilioController(ITwilioRestClient client)
+        private readonly string PhoneNumber;
+        public TwilioController(ITwilioRestClient client, IConfiguration config)
         {
             _client = client;
+            this.PhoneNumber = config["Twilio:PhoneNumber"] ?? "+19895850910";
         }
 
-        [HttpPost("PhoneNumber")]
-        public IActionResult SendSms(SmsMessageDTO req)
+
+        [HttpPost("notify/{ToPhoneNumber}/{MessageType}")]
+        public IActionResult SendSms(string ToPhoneNumber, string MessageType)
         {
+            if( MessageType is null )
+            {
+                return BadRequest("Please specify a message type.");
+            }
             var message = MessageResource.Create(
-                body: req.Body,
-                from: new Twilio.Types.PhoneNumber(req.From),
-                to: new Twilio.Types.PhoneNumber(req.To),
+                body: new MessageBuilder(MessageType).GetMessage(),
+                from: new Twilio.Types.PhoneNumber(PhoneNumber),
+                to: new Twilio.Types.PhoneNumber(ToPhoneNumber),
                 client: _client);
             return Ok("The following message was sent succesfully: \n" + message.Body);
         }
+
     }
 }
